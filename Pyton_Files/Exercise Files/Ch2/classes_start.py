20d2ff5c1a34f9925b6cb997973705397b146840
Brandon Geertson <brandongeertson@mycwi.cc>
2018-05-11 19:33:01 -0600

    F-Learn_python CH2 complete

--- Pyton_Files/Exercise Files/Ch2/classes_start.py
+++ Pyton_Files/Exercise Files/Ch2/classes_start.py
@@ -3,8 +3,31 @@
 #
 
 
-def main():
+class myClass():
+    def method1(self):
+        print("My class method")
 
-  
+    def method2(self, someString):
+        print("myClass method2 " + someString)
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someString):
+        print("anotherClass method2 " + someString)
+
+
+def main():
+    c = myClass()
+    c.method1()
+    c.method2("StringY!")
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("String here!")
+
+
 if __name__ == "__main__":
-  main()
+    main()
