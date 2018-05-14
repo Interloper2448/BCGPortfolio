bb9ab2d78f9c522ee0cd2ad54aba8942b1c1aabb
Brandon Geertson <brandongeertson@mycwi.cc>
2018-05-13 20:42:38 -0600

    f-Learn_Python Ch3 complete g-Added sandbox

--- Pyton_Files/murach/exercises/ch03/future_value.py
+++ Pyton_Files/murach/exercises/ch03/future_value.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
 
+def filterMonInp(statement):
+    while True:
+        inp = float(input(statement))
+        if inp <= 0:
+            print("Entry must be greater than 0")
+        else:
+            return inp
+
+def filterIntreInp(statement):
+    while True:
+        inp = float(input(statement))
+        if inp <= 0:
+            print("Entry must be greater than 0")
+        elif inp > 15:
+            print("Entry must be less than or equal to 15")
+        else:
+            return inp
+
+def filterYearsInp(statement):
+    while True:
+        inp = int(input(statement))
+        if inp <= 0:
+            print("Entry must be greater than 0 and less than or equal to 50")
+        elif inp > 50:
+            print("Entry must be greater than 0 and less than or equal to 50")
+        else:
+            return inp
+
+
 # display a welcome message
 print("Welcome to the Future Value Calculator")
 print()
@@ -8,9 +37,9 @@ choice = "y"
 while choice.lower() == "y":
 
     # get input from the user
-    monthly_investment = float(input("Enter monthly investment:\t"))
-    yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
-    years = int(input("Enter number of years:\t\t"))
+    monthly_investment = filterMonInp("Enter monthly investment:\t")
+    yearly_interest_rate = filterIntreInp("Enter yearly interest rate:\t")
+    years = filterYearsInp("Enter number of years:\t\t")
 
     # convert yearly values to monthly values
     monthly_interest_rate = yearly_interest_rate / 12 / 100
@@ -22,9 +51,10 @@ while choice.lower() == "y":
         future_value += monthly_investment
         monthly_interest_amount = future_value * monthly_interest_rate
         future_value += monthly_interest_amount
+        if i % 12 == 11:
+            print("Year = ", round(i/12), " Future Value = ", round(future_value, 2))
 
     # display the result
-    print("Future value:\t\t\t" + str(round(future_value, 2)))
     print()
 
     # see if the user wants to continue
--- Pyton_Files/murach/exercises/ch03/mpg.py
+++ Pyton_Files/murach/exercises/ch03/mpg.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 
-# display a welcome message
-print("The Miles Per Gallon application")
-print()
 
-# get input from the user
-miles_driven = float(input("Enter miles driven:         "))
-gallons_used = float(input("Enter gallons of gas used:  "))
+while True:
+    # display a welcome message
+    print("The Miles Per Gallon application")
+    print()
 
-if miles_driven <= 0:
-    print("Miles driven must be greater than zero. Please try again.")
-elif gallons_used <= 0:
-    print("Gallons used must be greater than zero. Please try again.")
-else:
-    # calculate and display miles per gallon
-    mpg = round((miles_driven / gallons_used), 2)
-    print("Miles Per Gallon:          ", mpg)
+    # get input from the user
+    miles_driven = float(input("Enter miles driven:\t\t\t"))
+    gallons_used = float(input("Enter gallons of gas used:\t\t"))
+    gallon_price = float(input("Enter cost per gallon:\t\t\t"))
 
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+    elif gallon_price <= 0:
+        print("The price of a gallon of gas must be greater than 0")
+    else:
+        # calculate and display miles per gallon
+        mpg = round((miles_driven / gallons_used), 2)
+        print("\nMiles Per Gallon:\t\t\t", mpg, sep="")
+        # Total Gas cost
+        gas_cost = round(gallons_used * gallon_price, 1)
+        print("Cost of used gas:\t\t\t", gas_cost, sep="")
+        # Cost per mile
+        mile_cost = round(gas_cost/miles_driven, 1)
+        print("The total cost per mile:\t\t", mile_cost, sep="")
+
+    inp = input("\n\rGet entries for another trip [y/n]?\t").lower()
+    if inp == "n":
+        break
 print()
 print("Bye")
-
-
-
